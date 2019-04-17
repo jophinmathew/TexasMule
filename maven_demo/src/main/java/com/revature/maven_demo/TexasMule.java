21effcbfbcc25c4c10f8277c5a4c3ee5ac0fcbc2
Jocelin <Jocelin@Jocelin>
2019-04-16 22:40:57 -0500

    First Commit

---
+++ maven_demo/src/main/java/com/revature/maven_demo/TexasMule.java
@@ -0,0 +1,20 @@
+package com.revature.maven_demo;
+
+public class TexasMule {
+
+	public TexasMule() {
+		// TODO Auto-generated constructor stub
+		String t1="Horse";
+		String t2="Donkey";
+		add(t1,t2);
+	}
+	public String add(String t1,String t2)
+	{
+		return t1+t2;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
